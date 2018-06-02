9ed19b7557c2e38e340af193dd84609c0093fa5c
Zach <zach@secondcloset.com>
2018-06-02 18:48:38 -0400

    Introdued modules, linting, routing, and direcctory structure

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Router } from "react-router-dom";
+import createBrowserHistory from "history/createBrowserHistory";
+import "./index.css";
+import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const history = createBrowserHistory();
+
+ReactDOM.render(
+    <Router history={history} >
+        <App />
+    </Router>
+, document.getElementById("root"));
+
 registerServiceWorker();
