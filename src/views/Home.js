535525a995cb9b4c09ed7491167b96adfbb59110
Zach <zach@secondcloset.com>
2018-06-03 16:19:22 -0400

    Working on animations for home page

--- src/views/Home.js
+++ src/views/Home.js
@@ -5,6 +5,8 @@ class Home extends React.Component {
     render() {
         return(
             <div className="container-fluid home">
+                <div className="circle"/>
+                
                 <div className="row">
                     <div className="col-xs-12 text-center">
                         <h1>hello</h1>
