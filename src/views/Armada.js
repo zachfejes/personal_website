7e8bf31a0ec210dc702a3e3e03243e9094f24fe6
Zach <zach@secondcloset.com>
2018-09-08 03:02:30 -0400

    Added text section

--- src/views/Armada.js
+++ src/views/Armada.js
@@ -2,59 +2,29 @@ import React from "react";
 //import PropTypes from "prop-types";
 import { Scatter } from "react-chartjs-2";
 
-import { SHIP_SCALE_DATA, SHIP_SCALE_UNITS } from "../config";
+import { SHIP_SCALE_DATA, SHIP_SCALE_OPTIONS } from "../config";
 import "./armada.css";
 
 class Armada extends React.Component {
     render() {
-
-        const options = {
-            legend: {
-                display: false
-            },
-            tooltips: {
-                callbacks: {
-                    label: (tooltipItem) => {
-                        return SHIP_SCALE_DATA.labels[tooltipItem.index] || "";
-                    }
-                }
-            },
-            scales: {
-                xAxes: [{
-                    scaleLabel: {
-                        display: true,
-                        labelString: "Canonical Length [m]"
-                    },
-                    ticks: {
-                        callback: function(value, index, values) {
-                            return value;
-                        }
-                    }
-                }],
-                yAxes: [{
-                    scaleLabel: {
-                        display: true,
-                        labelString: "Model Length [mm]"
-                    },
-                    ticks: {
-                        callback: function(value, index, values) {
-                            return value;
-                        }
-                    }
-                }]
-            }
-        };
-
         return(
             <div className="armada">
-                <div className="text-center">
-                    <h1>Armada Scale</h1>
+                <section className="titles">
+                    <h1>Armada Scaling</h1>
                     <br/>
                     <p>Analyzing the Non-Linear Scale for Use in Modeling</p>
-                </div>
+                </section>
 
-                <div className="graph">
-                    <Scatter data={SHIP_SCALE_DATA} options={options} />
+                <section className="graph">
+                    <Scatter data={SHIP_SCALE_DATA} options={SHIP_SCALE_OPTIONS} />
+                </section>
+
+                <div className="contentBlock">
+                    <p>
+                        This is a lot of stuff to consider! I wonder how I can use this information 
+                        to better inform my decision-making process when it comes to scaling my 
+                        models to be fitting for the game.
+                    </p>
                 </div>
             </div>
         );
